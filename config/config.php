40ba19858b24591d1f0dff1dcfde652e7a32e3b7
AzizJabri <azizjb.business@gmail.com>
2025-05-09 16:40:23 +0100

    updated env vars

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'event_db');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_HOST', $_ENV['AZURE_MYSQL_HOST'] ?? 'localhost');
+define('DB_NAME', $_ENV['AZURE_MYSQL_DBNAME'] ?? 'event_db');
+define('DB_USER', $_ENV['AZURE_MYSQL_USERNAME'] ?? 'root');
+define('DB_PASS', $_ENV['AZURE_MYSQL_PASSWORD'] ?? '');
 define('DB_CHARSET', 'utf8mb4');
 
 define('BASE_URL', 'http://localhost/php/venue-booking');
